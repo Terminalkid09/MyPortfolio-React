6b3c5b2b8956e15551b305cc58510719ae08eb3c
CL <ilysmmyll@gmail.com>
2026-09-18 15:26:53 +0200

    Remove roadmap section from portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Skills from "../components/Skills.jsx";
 import Projects from "../components/Projects";
-import Roadmap from "../components/Roadmap";
 import Contact from "../components/Contact.jsx";
 import Footer from "../components/Footer";
 
@@ -16,7 +15,6 @@ export default function Home() {
       <About />
       <Skills />
       <Projects />
-      <Roadmap />
       <Contact />
       <Footer />
      
